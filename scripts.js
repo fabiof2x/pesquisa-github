aa0e098803d50ec42f080167daeceee32a91d547
fabiof2x <fabioar@gmail.com>
2021-08-15 12:55:41 -0300

    modularizando o código

--- scripts.js
+++ scripts.js
@@ -1,56 +1,76 @@
-const listElement = document.querySelector('#list');
-const searchInput = document.querySelector('#search');
-const languageSelect = document.querySelector('#language-tags');
+const Template = (function () {
+  const languageSelect = document.querySelector('#language-tags');
+  const listElement = document.querySelector('#list');
 
-let languageTag = 'en-US';
+  let listItems = [];
+  let languageTag = 'en-US';
 
-let listItems = [
-  {
-    full_name: 'Javascript 1',
-    created_at: '2020-07-25T20:10:50',
-    forks: 15300
-  },
-  {
-    full_name: 'Javascript 2',
-    created_at: '2020-07-25T20:10:50',
-    forks: 15300
-  },
-  {
-    full_name: 'Javascript 3',
-    created_at: '2020-07-25T20:10:50',
-    forks: 15300
+  languageSelect.addEventListener('change', changeLanguage);
+
+  function changeLanguage() {
+    languageTag = languageSelect.value;
+    render();
   }
-]
 
-languageSelect.addEventListener('change', changeLanguage);
+  function setList(list) {
+    listItems = list;
+    render();
+  }
 
-function changeLanguage() {
-  languageTag = languageSelect.value;
-  render();
-}
+  function render() {
+    let html = '';
+    const numberFormatter = new Intl.NumberFormat(languageTag);
+    const dateFormatter = new Intl.DateTimeFormat(languageTag, { week: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    listItems.forEach(item => {
+      const forks = numberFormatter.format(item.forks);
+      const createdAt = dateFormatter.format(new Date(item.created_at));
+      html += `
+          <li>
+            <div>
+              <b>Nome:</b> ${item.full_name}
+            </div>
+            <div>
+              <b>Criado em:</b> ${createdAt}
+            </div>
+            <div>
+              <b>Forks:</b> ${forks}
+            </div>
+          </li>
+        `;
+    })
+    listElement.innerHTML = html;
+  }
 
-function render() {
-  let html = '';
-  const numberFormatter = new Intl.NumberFormat(languageTag);
-  const dateFormatter = new Intl.DateTimeFormat(languageTag, { week: 'long', year: 'numeric', month: 'long', day: 'numeric' });
-  listItems.forEach(item => {
-    const forks = numberFormatter.format(item.forks);
-    const createdAt = dateFormatter.format(new Date(item.created_at));
-    html += `
-      <li>
-        <div>
-          <b>Nome:</b> ${item.full_name}
-        </div>
-        <div>
-          <b>Criado em:</b> ${createdAt}
-        </div>
-        <div>
-          <b>Forks:</b> ${forks}
-        </div>
-      </li>
-    `;
-  })
-  listElement.innerHTML = html;
-}
+  return {
+    setList
+  }
 
-render();
+})();
+
+const Data = (function ($) {
+  const searchInput = document.querySelector('#search');
+
+  searchInput.addEventListener('keyup', search);
+
+  function search(event) {
+    if (event && event.keyCode === 13) {
+      $.setList([
+        {
+          full_name: 'Javascript 1',
+          created_at: '2020-07-25T20:10:50',
+          forks: 15300
+        },
+        {
+          full_name: 'Javascript 2',
+          created_at: '2020-07-25T20:10:50',
+          forks: 15300
+        },
+        {
+          full_name: 'Javascript 3',
+          created_at: '2020-07-25T20:10:50',
+          forks: 15300
+        }
+      ]);
+    }
+  }
+})(Template)
