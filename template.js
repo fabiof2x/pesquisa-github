300445f98c5beb231c1ef240060fe31eea29e97f
fabiof2x <fabioar@gmail.com>
2021-08-16 12:29:23 -0300

    reagindo a mudanças com proxies

--- template.js
+++ template.js
@@ -2,25 +2,34 @@ const languageSelect = document.querySelector('#language-tags');
 const listElement = document.querySelector('#list');
 const templateWorker = new Worker('./template_worker.js');
 
-const config = {
+const config = new Proxy({
   listItems: [],
   languageTag: 'en-US'
-}
+}, {
+  set: function (target, prop, value, receiver) {
+    if (prop === 'listItems' || prop === 'languageTag') {
+      //Reflect.set(target, prop, value, receiver);
+      Reflect.set(...arguments);
+      render();
+      return true;
+    }
+    return false;
+  }
+})
 
 languageSelect.addEventListener('change', changeLanguage);
 
 function changeLanguage() {
   config.languageTag = languageSelect.value;
-  render();
 }
 
 export function setList(list) {
   config.listItems = list;
-  render();
 }
 
 function render() {
-  templateWorker.postMessage(config);
+  const configParam = JSON.parse(JSON.stringify(config));
+  templateWorker.postMessage(configParam);
 
   templateWorker.onmessage = function ({ data }) {
     listElement.innerHTML = data;
